0573f76ed9de7f5cc372fdd2dfffed18e829666d
light-zhao <2568252796@qq.com>
2017-02-10 18:43:31 +0800

    1.0.8+

---
+++ Assets/scripts/ApplyLanguage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using I2.Loc;
+
+public class ApplyLanguage : MonoBehaviour {
+	SetLanguage sl;
+	// Use this for initialization
+	void Start () {
+		sl = GetComponent<SetLanguage> ();
+		sl.ApplyLanguage ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
