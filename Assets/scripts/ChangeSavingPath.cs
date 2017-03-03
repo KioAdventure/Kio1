15f20270870ef809342f3eb5dedf8737c736109a
light-zhao <2568252796@qq.com>
2017-03-03 11:06:07 +0800

    1.0.8++

---
+++ Assets/scripts/ChangeSavingPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeSavingPath : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+//		ES2GlobalSettings.defaultPCDataPath;
+		string path=System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+		path+="\\Spacelight\\Kio1\\";
+		ES2GlobalSettings.defaultPCDataPath = path;
+		Debug.Log(path+" "+ES2GlobalSettings.defaultPCDataPath);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
