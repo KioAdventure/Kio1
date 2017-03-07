00de7e2937e07771e34ea24064484d8738c4ea00
light-zhao <2568252796@qq.com>
2017-03-07 10:00:19 +0800

    1.0.9

--- Assets/scripts/ChangeSavingPath.cs
+++ Assets/scripts/ChangeSavingPath.cs
@@ -8,10 +8,11 @@ public class ChangeSavingPath : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 //		ES2GlobalSettings.defaultPCDataPath;
+		ES2.Init();
 		string path=System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
 		path+="\\Spacelight\\Kio1\\";
 		ES2GlobalSettings.defaultPCDataPath = path;
-		Debug.Log(path+" "+ES2GlobalSettings.defaultPCDataPath);
+		Debug.Log(ES2GlobalSettings.defaultMacDataPath+" "+ES2GlobalSettings.defaultPCDataPath);
 
 	}
 	
