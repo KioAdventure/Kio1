3dc3f4d08d2f488320ec5756ed7c7fd23180df70
light-zhao <2568252796@qq.com>
2017-03-30 16:38:02 +0800

    1.1.2

---
+++ Assets/scripts/RestAllAchievement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class RestAllAchievement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void reset(){
+		if (SteamManager.Initialized) {
+			SteamUserStats.ResetAllStats (true);
+		}
+	}
+}
