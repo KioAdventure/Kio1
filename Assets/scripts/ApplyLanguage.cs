5bf564f332d4704755c9a45fb1c5fcdcf9f88ab6
light-zhao <2568252796@qq.com>
2017-04-02 12:11:33 +0800

    1.1.3

--- Assets/scripts/ApplyLanguage.cs
+++ Assets/scripts/ApplyLanguage.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using I2.Loc;
+
 public class ApplyLanguage : MonoBehaviour {
 	public string defaultLanguage = "English";
 	public bool autoChange=true;
 	// Use this for initialization
 	void Start () {
+//		bool init= ES2.Load<bool> ("profile.dat?tag=init");
+//		print (init);
+		if(ES2.Exists("profile.dat?tag=init"))
+			return;
 		if( LocalizationManager.HasLanguage(defaultLanguage))
 		{
 			LocalizationManager.CurrentLanguage = defaultLanguage;
 		}
 		string l = Application.systemLanguage.ToString();
 		print (l);
+		ES2.Save<bool> (true,"profile.dat?tag=init");
 		if(!autoChange)
 		return;
 		if( LocalizationManager.HasLanguage(l))
